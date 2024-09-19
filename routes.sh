98020e6f707c86881c56fc15aaac973f88245d49
SLitvinov <slitvinov@sportmasterlab.net>
2024-09-19 12:52:56 +0300

    routes.sh

--- routes.sh
+++ routes.sh
@@ -5,6 +5,6 @@ for filename in "$search_dir"/*.txt
 do
   echo "file = $filename"
   while read -r line; do
-      echo "ip rule add from 10.66.66.0/24 to $line lookup 210"
+      ip rule add from 10.66.66.0/24 to $line lookup 210
   done < "$filename"
 done
